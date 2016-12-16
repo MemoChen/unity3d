5b3f9c139eaee7e8bf9d78e65ec5218f1d4891ed
MemoChen <1049105081@qq.com>
2016-12-16 11:24:52 +0800

    FPS new

---
+++ UI/FPSManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class FPSManager : MonoBehaviour
+{
+    private Text textField;
+    private float fps = 0;
+
+    void Awake()
+    {
+        textField = GetComponent<Text>();
+    }
+
+    void LateUpdate()
+    {
+        string text = null;
+        float interp = Time.deltaTime / (0.5f + Time.deltaTime);
+        float currentFPS = 1.0f / Time.deltaTime;
+        fps = Mathf.Lerp(fps, currentFPS, interp);
+        text += "FPS:" + Mathf.RoundToInt(fps);
+        textField.text = text;
+    }
+}
