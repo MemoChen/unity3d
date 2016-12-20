19b297588f4c09d58504682cb6034fa3e8813a5a
MemoChen <1049105081@qq.com>
2016-12-20 16:17:34 +0800

    scale sumbit

---
+++ PlayerController/ScaleController.cs
@@ -0,0 +1,37 @@
+﻿///
+///朝单一方向缩放物体
+///position.z=A;
+///scale.z=2*A;
+///
+
+using UnityEngine;
+using System.Collections;
+
+public class ScaleController : MonoBehaviour
+{
+
+    float time = 0;
+    float index = 0;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time < 3.5f && time > 1.5f)
+        {
+            transform.localScale = new Vector3(0.7f, 0.75f, -2 * time);
+            transform.localPosition = new Vector3(0, 0, -time);
+            index = time;
+        }
+        if (time > 3.5f)
+        {
+            index -= Time.deltaTime;
+            transform.localScale = new Vector3(0.7f, 0.75f, -2 * index);
+            transform.localPosition = new Vector3(0, 0, -index);
+        }
+        if (time > 6f)
+        {
+            time = 0;
+        }
+
+
+    }
+}
